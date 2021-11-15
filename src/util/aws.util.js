4eca8382c5be72db7c1027ebdd2ca79fba28ff28
Felipe Nuñez <felipenunez@M-C02G507MMD6Q.local>
2021-11-15 16:17:08 -0300

    refactor():prettier

--- src/util/aws.util.js
+++ src/util/aws.util.js
@@ -1,9 +1,9 @@
-import AWS from 'aws-sdk'
+import AWS from "aws-sdk";
 
 /**
  * Return a new S3 service
  * @param {*} credentials AWS Credentials
  */
-export function makeS3service(credentials){
-    return new AWS.S3({ credentials: credentials });
+export function makeS3service(credentials) {
+  return new AWS.S3({ credentials: credentials });
 }
